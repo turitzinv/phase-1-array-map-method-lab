40ad8fb7b1e624c5474db2702cb4e48915762509
Victor Turitzin <turitzinv@gmail.com>
2021-12-08 20:01:51 -0500

    commit tree 1e9763ed2e18a859c8980d8bf91290c947abd922

--- index.js
+++ index.js
@@ -11,6 +11,53 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
+const titleCased = () => tutorials.map(tutorial => {
+  return tutorial
+    .split(' ')
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(' ')
+})
+
+console.log(titleCased)
+
+/*
+const titleCased = (array) => {
+  array.map(string => string.trim().split(" "))
+
   return tutorials
 }
+console.log(titleCase)
+*/
+
+/*
+function upperCase(array) {
+  for (i = 0; i < array.length; i++){
+  return array[i].toLowerCase().split(" ").map( word => {
+    return word[0].toUpperCase() + word.substring(1)
+  }).join (" ")
+  }
+}
+
+console.log(upperCase(tutorials))
+/*
+
+//Below code only makes the first element an array with each word being its own element
+/*
+stringArray = function (array) {
+  for (element of array) {
+    return element.trim().split(" ")
+  }
+}  
+*/
+
+//Below code returns ['W']
+/*
+function capitalLetter(array){
+  for (element of array) {
+    return element.charAt(0).toUpperCase()
+  }
+
+}
+
+console.log(stringArray(capitalLetter(tutorials)))
+*/
